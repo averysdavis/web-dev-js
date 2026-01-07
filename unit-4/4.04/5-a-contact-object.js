04be7d31bf572d10b334c76ce2895ab53609a9e3
Avery Davis <averydavis9@proton.me>
2026-01-07 13:45:08 -0800

    4.04.5

---
+++ unit-4/4.04/5-a-contact-object.js
@@ -0,0 +1,9 @@
+var contact = {
+    firstName: "Emily",
+    lastName: "R",
+    phoneNumber: "856-222-1014"
+};
+
+println(contact.firstName);
+println(contact.lastName);
+println(contact.phoneNumber);
